3db18caa080bc94e7d09355c087fbbea212126d1
AriCoder <chelyara@gmail.com>
2018-05-28 19:15:21 -0500

    se agregan cambios a slide

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -1,8 +1,12 @@
 (function(){
   var initslider = function(){
     $('.slider').slick({
+      autoplay:true,
+      autoplaySpeed: 2000,
       arrows: false,
       infinite: true,
+      pauseOnHover: false,
+      pauseOnFocus: false,
       slidesToShow: 1,
       slidesToScroll: 1,
       dots: true
